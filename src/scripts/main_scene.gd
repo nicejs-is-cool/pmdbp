45e97ea241101fd3b914dfe1e9dac6312b4748eb
nicejs-is-cool <nicejsalt@gmail.com>
2026-09-07 13:37:11 -0300

    start working on dungeon stuff

--- src/scripts/main_scene.gd
+++ src/scripts/main_scene.gd
@@ -9,3 +9,7 @@ func _ready():
 	portrait.texture = ptxt
 func _process(_delta: float):
 	pass
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	print("entered")
+	get_tree().change_scene_to_file.call_deferred("res://src/scenes/dungeon_main.tscn")
