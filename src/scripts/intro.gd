8a75313ba7b422c6c07e794294cbe229a5ed77ab
nicejs-is-cool <nicejsalt@gmail.com>
2026-09-06 23:27:18 -0300

    particles !!

--- src/scripts/intro.gd
+++ src/scripts/intro.gd
@@ -2,7 +2,13 @@ extends Node
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready():
-	animation_player.play("Move")
+	animation_player.play(&"move")
+	pass
 
 func _process(_delta: float):
 	pass
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == &"move":
+		get_tree().change_scene_to_file("res://src/scenes/main.tscn")
